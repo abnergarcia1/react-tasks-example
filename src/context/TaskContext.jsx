3c4bd96c6f4692b45c7442cf4ff555d53e92a6b8
Abner Garcia <abner.garciaruiz@hootsuite.com>
2022-10-21 18:11:41 -0400

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  const createTask = (task) => {
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length,
+        title: task.title,
+        description: task.description,
+      },
+    ]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id != id));
+  };
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
